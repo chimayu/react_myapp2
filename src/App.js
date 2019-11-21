e0017eca3cedef97c9358f3aa5a98e341665226e
mayumi <chimayu@gmail.com>
2019-11-21 23:17:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function mappingState(state){
+  return state;
 }
+
+class App extends Component {
+  constructor(props) {
+    super();
+  }
+  render(){
+    return (
+      <div>
+        <Message />
+        <Button />
+      </div>
+    )
+  }
+}
+
+App = connect()(App);
+
+class Message extends Component {
+  render(){
+    return (
+      <p>
+        {this.props.message} : 
+        {this.props.counter}
+      </p>
+    )
+  }
+}
+
+Message = connect(mappingState)(Message);
+
+class Button extends Component {
+  constructor(props){
+    super();
+    this.doAction = this.doAction.bind(this);
+  }
+  doAction(e){
+    if(e.shiftKey){
+      this.props.dispatch({type:'DECREMENT'})
+    } else {
+      this.props.dispatch({type:'INCREMENT'})
+    }
+    
+  }
+  render(){
+    return (
+      <button onClick={this.doAction}>Click!!</button>
+    )
+  }
+
+}
+
+Button = connect()(Button);
 
 export default App;
